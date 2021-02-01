3d5abc08f100443322347c5e234c2e983ee85204
jagerking <darthsoodo@gmail.com>
2021-02-01 02:43:33 -0500

    updated IP gettering stuff

--- ipsample.py
+++ ipsample.py
@@ -1,15 +1,28 @@
 import socket;
 import smtplib;
+import fcntl
+import struct
 
 ip = socket.gethostbyname(socket.gethostname());
 
+remote_server = "google.com"
+def get_my_ip_address(remote_server):
+    """
+    Return the/a network-facing IP number for this system.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
+        s.connect((remote_server, 80))
+        return s.getsockname()[0]
+
+ip2 =get_my_ip_address(remote_server)
+
 #replace the x's with your info!
 #remember to allow third party api's to use your senderAddress gmail account!
-senderAddress = "xxx@gmail.com";
-recieverAddress = "xxxxx";
-senderPassword = "xxxxx";
+senderAddress = "racistclarkson@gmail.com";
+recieverAddress = "darthsoodo@gmail.com";
+senderPassword = "WRHS1184";
 subject = "Raspberry Pi Local IP";
-body = "The IP address is: " + ip;
+body = "The IP address is: " + ip + " | "+ ip2;
 
 smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465);
 smtp_server.login(senderAddress, senderPassword);
